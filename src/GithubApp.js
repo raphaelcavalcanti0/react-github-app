54571a0c7bf5d591b4c10e511e74437c7f293463
Raphael Cavalcanti <46363242+raphaelcavalcanti0@users.noreply.github.com>
2021-12-29 22:04:58 -0300

    Create project

---
+++ src/GithubApp.js
@@ -0,0 +1,17 @@
+import Profile from "./components/Profile";
+import Repos from "./components/Repos";
+
+const GithubApp = () => {
+  return (
+    <>
+      <Profile>
+        infos of profile
+      </Profile>
+      <Repos>
+        infos of repos
+      </Repos>
+    </>
+  );
+}
+
+export default GithubApp;
--- src/GithubApp.test.js
+++ src/GithubApp.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import GithubApp from './GithubApp';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<GithubApp />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
